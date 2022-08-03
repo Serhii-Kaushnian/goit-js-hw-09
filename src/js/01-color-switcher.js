851d8debb39475aba0f6737e0bb1dd6a91e88c84
Serhii Kaushnian <skaushnjan@gmail.com>
2022-08-03 23:26:01 +0200

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,18 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startButtonRef = document.querySelector('[data-start]');
+const stopButtonRef = document.querySelector('[data-stop]');
+
+function changeBodyColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+startButtonRef.addEventListener('click', () => {
+  timerId = setInterval(changeBodyColor, 1000);
+  document.querySelector('[data-start]').disabled = true;
+});
+stopButtonRef.addEventListener('click', () => {
+  clearInterval(timerId);
+  document.querySelector('[data-start]').disabled = false;
+});
