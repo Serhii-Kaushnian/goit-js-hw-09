a0b891956601ea9c70d05e7d2cd345f9d1e06459
Serhii-Kaushnian <54234797+Serhii-Kaushnian@users.noreply.github.com>
2022-08-04 18:41:16 +0000

    Deploying to gh-pages from @ Serhii-Kaushnian/goit-js-hw-09@d9dc912a1443320059d0e983613209695f336580 🚀

---
+++ 01-color-switcher.820228cb.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");function r(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}function n(){clearInterval(timerId),document.querySelector("[data-start]").disabled=!1,e.removeEventListener("click",n)}t.addEventListener("click",(function(){timerId=setInterval(r,1e3),document.querySelector("[data-start]").disabled=!0,e.addEventListener("click",n)}))}();
+//# sourceMappingURL=01-color-switcher.820228cb.js.map
