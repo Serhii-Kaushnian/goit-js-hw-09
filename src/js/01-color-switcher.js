f1328c476466b0b1e29e1014dc8e0ba1b9e90701
Serhii Kaushnian <skaushnjan@gmail.com>
2022-08-04 20:43:50 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,7 +1,7 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-
+let timerId;
 const startButtonRef = document.querySelector('[data-start]');
 const stopButtonRef = document.querySelector('[data-stop]');
 
