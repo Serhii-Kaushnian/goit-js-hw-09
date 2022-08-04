90310e95e47314c42443c447390e5f12af878b4c
Serhii Kaushnian <skaushnjan@gmail.com>
2022-08-04 20:34:51 +0200

    task 2 completed

---
+++ src/js/02-timer.js
@@ -0,0 +1,71 @@
+const { default: flatpickr } = require('flatpickr');
+import Notiflix from 'notiflix';
+
+const dateInput = document.querySelector('#datetime-picker');
+const startButtonRef = document.querySelector('[data-start]');
+const timerDivRef = document.querySelector('.timer');
+const daysSpanRef = document.querySelector('[data-days]');
+const hoursSpanRef = document.querySelector('[data-hours]');
+const minutesSpanRef = document.querySelector('[data-minutes]');
+const secondsSpanRef = document.querySelector('[data-seconds]');
+
+startButtonRef.disabled = true;
+let intervalId;
+let deltaTime;
+
+const currentTime = new Date();
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (!selectedDates[0]) {
+      return;
+    }
+    deltaTime = selectedDates[0] - currentTime;
+    if (deltaTime <= 0) {
+      alertMessage();
+    } else if (deltaTime > 0) {
+      Notiflix.Notify.success('Now You can start Coundown');
+      startButtonRef.disabled = false;
+      dateInput.disabled = true;
+      basicMarkUp(deltaTime);
+    }
+  },
+};
+const datePicker = flatpickr(dateInput, options);
+
+function alertMessage() {
+  startButtonRef.disabled = true;
+  Notiflix.Notify.failure('Please choose a date in the future');
+}
+
+function basicMarkUp(deltaTime) {
+  const seconds = Math.floor(deltaTime / 1000) % 60;
+  const minutes = Math.floor(deltaTime / 1000 / 60) % 60;
+  const hours = Math.floor(deltaTime / 1000 / 60 / 60) % 24;
+  const days = Math.floor(deltaTime / 1000 / 60 / 60 / 24);
+
+  daysSpanRef.textContent = days < 10 ? `0${days}` : days;
+  hoursSpanRef.textContent = hours < 10 ? `0${hours}` : hours;
+  minutesSpanRef.textContent = minutes < 10 ? `0${minutes}` : minutes;
+  secondsSpanRef.textContent = seconds < 10 ? `0${seconds}` : seconds;
+}
+
+startButtonRef.addEventListener('click', () => {
+  intervalId = setInterval(startCounting, 1000);
+});
+
+function startCounting() {
+  startButtonRef.disabled = true;
+  if (deltaTime < 2000) {
+    dateInput.disabled = false;
+    clearInterval(intervalId);
+    basicMarkUp(deltaTime);
+    timerDivRef.style.backgroundColor = 'green';
+  }
+  deltaTime = deltaTime - 1000;
+
+  basicMarkUp(deltaTime);
+}
